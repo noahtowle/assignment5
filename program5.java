af019385dd482635c75d5a01ab47b3e79ce83f6b
noahtowle <160976682+noahtowle@users.noreply.github.com>
2024-03-04 16:37:59 -0500

    updated to print file argument for testing

--- program5.java
+++ program5.java
@@ -12,24 +12,11 @@ public class program5 {
         // C:/Users/Noah/eclipse-workspace/assignment2/src/accidents.csv
     	String csvFilePath = args[0];
         
-        //printFile(reports);
+    	MyBinarySearchTree reports = new ReportReader(csvFilePath).readReports();
+        reports.inorder();
         
     }
 
-	
-	private static void printFile(ArrayList<Report> reports) {
-        for (Report report : reports) {
-    		System.out.println("Start Time: " + report.getStart_time());
-    		System.out.println("Date: " + report.getDate());
-    		System.out.println("County: " + report.getCounty());
-    		System.out.println("State: " + report.getState());
-    		System.out.println("--------------------------------");
-        }
-	}
-
-
-		
-	
 }
 
 	
